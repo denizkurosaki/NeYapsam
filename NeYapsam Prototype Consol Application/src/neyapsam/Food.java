fa0260600324127449f6b80466dbe9a475b0c09a
Ahmet Kurt <AhmeettKurt@gmail.com>
2018-11-29 22:12:29 +0300

    Update Food.java

--- NeYapsam Prototype Consol Application/src/neyapsam/Food.java
+++ NeYapsam Prototype Consol Application/src/neyapsam/Food.java
@@ -71,7 +71,7 @@ public class Food {
                 arr[i] = sc.next();
             }
             
-            ResultSet rs = stmt.executeQuery("select name from Food where Ingredients.name = '" + arr[0] + " and " + arr[1] + " and " + arr[2]);
+            ResultSet rs = stmt.executeQuery("select f.name from Food as f,Ingredient as i where i.name = '" + arr[0] + " and " + arr[1] + " and " + arr[2]);
             System.out.println("fid \t f_name");
             while (rs.next()) {
                 System.out.println(rs.getString(1));
