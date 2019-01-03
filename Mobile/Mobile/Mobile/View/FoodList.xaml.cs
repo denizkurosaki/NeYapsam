f0b72e28428dc53d0688cbaa3e22da300d6c4a2f
Ömer KAYA <fethullahomerkaya@anadolu.edu.tr>
2019-01-03 23:13:42 +0300

    yeni referanslar eklendi

--- Mobile/Mobile/Mobile/View/FoodList.xaml.cs
+++ Mobile/Mobile/Mobile/View/FoodList.xaml.cs
@@ -1,10 +1,4 @@
-﻿using Mobile.Controller;
-using Mobile.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
